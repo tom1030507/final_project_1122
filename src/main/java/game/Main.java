9097707e8a896198815e82236dcff145b29540ca
qmzp93 <b31901937@gmail.com>
2024-05-28 23:55:48 +0800

    test3

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,8 +1,24 @@
 package game;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.text.Text;
+
+public class Main extends Application {
+
+  public void start(Stage primaryStage) {
+    Pane pane=new Pane();
+    Text text=new Text(200,150,"hello world!");
+    pane.getChildren().add(text);
+    // Create a scene and place it in the stage
+    Scene scene = new Scene(pane, 500, 300);
+    primaryStage.setTitle("SliderDemo"); // Set the stage title
+    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.show(); // Display the stage   
+  }
     public static void main(String[] args) {
-        System.out.println("Hello, Worldsahdkahj!");
-        System.out.println("Hi");
+      launch(args);
     }
 }
