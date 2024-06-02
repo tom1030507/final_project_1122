952b8ef832bb8e5abb831a7eae67e79f58459af3
qmzp93 <b31901937@gmail.com>
2024-06-02 21:11:41 +0800

    camera_tset

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -2,20 +2,30 @@ package game;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.scene.ParallelCamera;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	boolean moveRight=false, moveLeft=false;
+    boolean moveRight=false, moveLeft=false;
 
 	public void start(Stage primaryStage) {
-		Pane pane=new Pane();
+        ImageView test = new ImageView(new Image(getClass().getResourceAsStream("ttt.jpg")));
+        test.setFitWidth(1300);
+        test.setFitHeight(700);
+        test.setTranslateX(-250);
+        test.setTranslateY(-300);
+
+        Pane pane=new Pane();
 		Character character=new Character(50,300);
         Enemy pig=new Enemy(200, 314, 800, 314);
 		pig.setTargetPlayer(character);
+
         character.setOnKeyPressed(e -> {
             if (e.getCode() == KeyCode.RIGHT) {
                 moveRight = true;
@@ -37,6 +47,7 @@ public class Main extends Application {
                 character.stop();
             }
         });
+       
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
@@ -47,17 +58,27 @@ public class Main extends Application {
                     character.move_left();
                 }
                 character.applyGravity();
+                character.imageview.setTranslateX(character.getX()+1);
+                character.imageview.setTranslateX(character.getX()-1);
             }
         };
         timer.start();
-		pane.getChildren().addAll(character,pig);
-        
-		Scene scene = new Scene(pane, 1300, 700);
+		pane.getChildren().addAll(test,character,pig);
+
+        Scene scene = new Scene(pane, 1300, 700);
+
+        ParallelCamera camera = new ParallelCamera();
+        scene.setCamera(camera);
+        // //绑定摄像机的位置到玩家的位置
+        camera.translateXProperty().bind(character.imageview.translateXProperty().subtract(scene.getWidth() / 2-100));
+        camera.translateYProperty().bind(character.imageview.translateYProperty().subtract(scene.getHeight() / 2+100));
+
 		primaryStage.setTitle("test"); 
 		primaryStage.setScene(scene); 
 		primaryStage.show();
 
 		character.requestFocus();
+        character.setFocusTraversable(true);
 	}
     
     public static void main(String[] args) {
