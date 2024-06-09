fc3342f8c750ad260b8c6a797c6fd50c28628ee4
Tom <tom1030507@gmail.com>
2024-06-09 21:32:07 +0800

    add media configuration

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -3,7 +3,6 @@ package game;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 public class Main extends Application {
 
     private static int level = 2;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module game {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
+    requires transitive javafx.media;
 
     opens game to javafx.fxml;
     exports game;
