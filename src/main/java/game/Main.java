fd621b61283a17996aaddf41005256594f3736cc
Tom <tom1030507@gmail.com>
2024-06-09 16:33:39 +0800

    add levelchange function

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -7,8 +7,14 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     private static int level = 2;
+    private static Stage primaryStage;
 
-	public void start(Stage primaryStage) {
+	public void start(Stage Stage) {
+        primaryStage = Stage;
+        launchLevel();
+	}
+
+    public static void launchLevel() {
         switch (level) {
             case 0:
                 Background menu = new Menu();
@@ -32,8 +38,13 @@ public class Main extends Application {
                 primaryStage.show();
                 break;
         }
-	}
+    }
     
+    public static void setLevel(int newLevel) {
+        level = newLevel;
+        launchLevel();
+    }
+
     public static void main(String[] args) {
       	launch(args);
     }
